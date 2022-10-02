57cd82870f3f5bc8a841d6b19c0d267670fa0fe0
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-10-02 22:04:12 +0300

    Update bot.py

--- bot/misc/bot.py
+++ bot/misc/bot.py
@@ -1,7 +1,7 @@
 from os import environ
 from aiogram import Bot
 
-API_TOKEN = "5478037139:AAGEMwmj5IjclMzr0IRbbt79bq8zrzwm-0E"
+API_TOKEN = ""
 PARSE_MODE = "HTML"
 
 bot = Bot(token = API_TOKEN, parse_mode=PARSE_MODE)
