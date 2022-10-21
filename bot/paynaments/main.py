bf71514b1876520f9d2d794ad841f0b4b5f189ec
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-10-21 11:28:51 +0300

    Add files via upload

---
+++ bot/paynaments/main.py
@@ -0,0 +1,18 @@
+from .wayforpay import get_invoice_url
+from aiogram import F
+from aiogram import Router
+from bot.filters.in_black_list import InBlacklist
+from bot.filters.user_exist import UserExistFilter
+from aiogram import types
+from ..keyboards.inline import get_guard_access_markup
+
+paynament_router = Router()
+
+
+@paynament_router.callback_query(F.data == 'get_guard_access', InBlacklist(in_blacklist = False), UserExistFilter(user_exist = True))
+async def alarm_handler(query: types.CallbackQuery):
+    user_id = query.from_user.id
+    url = get_invoice_url(user_id)
+    
+    keyboard_markup = get_guard_access_markup(user_id, url)
+    await query.message.answer("Тепер перейдіть по посиланню для оплати послуги:", reply_markup = keyboard_markup)
---
+++ bot/paynaments/wayforpay.py
@@ -0,0 +1,39 @@
+import datetime
+import requests
+import hmac
+import hashlib
+
+
+merchant_account = 'freelance_user_634c73f3cc45b'
+merchant_key = "704328eab72fb02d83be3663f8cc9e88b7cb64a1"
+merchant_domain = 'edossecurity.com'  
+generate_signature = lambda merchant_key, data_str: hmac.new(merchant_key.encode('utf-8'), data_str.encode('utf-8'), hashlib.md5).hexdigest()
+
+
+def get_invoice_url(user_id: int) -> str:
+    order_date = str(round(datetime.datetime.now().timestamp()))
+    signature_data = f"{merchant_account};{merchant_domain};{order_date}:{user_id};1421412898;1;UAH;Виклик охорони;1;1"
+   
+    body = {
+        "transactionType": "CREATE_INVOICE",
+        "merchantAccount": merchant_account,
+        "merchantDomainName": merchant_domain,
+        "merchantSignature": generate_signature(merchant_key, signature_data),
+        "apiVersion": 1,
+        "language": "ua",
+        "serviceUrl": "https://44be-194-44-56-49.eu.ngrok.io",
+        "orderReference": f"{order_date}:{user_id}",
+        "orderDate": 1421412898,
+        "amount": 1, 
+        "currency": "UAH",
+        "orderTimeout": 86400,
+        "productName": ["Виклик охорони"],
+        "productPrice": [1],
+        "productCount": [1],
+    }
+
+    r = requests.get('https://api.wayforpay.com/api', json = body)
+    data = r.json()
+    return data["invoiceUrl"]
+
+
