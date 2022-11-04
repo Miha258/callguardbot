c47d9d3accf9f3f60f2445577d2374d230bceafd
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-11-04 15:06:16 +0200

    Delete bot/handlers directory

--- bot/handlers/user/account/state.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.state import State, StatesGroup
-
-
-class AccountEdits(StatesGroup):
-    fullname = State()
-    photo = State()
-    description = State()
-    city = State()
