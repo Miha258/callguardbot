4b8de7660eb1a2309ad3836d871f2f09f73ab64f
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-12-11 02:49:45 +0200

    Delete bot/misc/utils/utils directory

--- bot/misc/utils/utils/admin.py
+++
@@ -1,9 +0,0 @@
-import json
-from .main import JSON_PATH
-
-
-def admin_exists(user_id: int):
-    with open(JSON_PATH, 'r', encoding = 'utf-8') as f:
-        config = json.load(f)
-
-    return user_id in config['admins']
