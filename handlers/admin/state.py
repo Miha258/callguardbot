d3aa26a0079a8c83fa8859506c6b711f8bbec79d
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-12-11 02:53:09 +0200

    Delete handlers directory

--- handlers/admin/state.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class AdminState(StatesGroup):
-    add_city = State()
-    remove_city = State()
-    ban_user = State()
--- handlers/user/alarm/state.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class AlarmState(StatesGroup):
-    amount_of_guards = State()
-    explain_reason = State()
-    get_user_location = State()
-    user_respond = State()
